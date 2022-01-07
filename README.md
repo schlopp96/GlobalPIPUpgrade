01a3036143f912e310abf33cda184fceaa1be59f
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-01-07 04:19:13 -0600

    Initial Commit
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+# Total PIP-Package Upgrade
+
+- This is a small powershell script that will allow the user to upgrade _all_ pip packages at once.
+
+## Usage
+
+- Simply run the script using powershell, and press "1" when prompted, then press the [ENTER] key to confirm your choice.
+  - From here, the upgrading process will continue on its own until its completion, where it will close the console window on its own.
---
+++ src/totalupgrade_pip.ps1
@@ -0,0 +1,20 @@
+#!usr/bin/env pwsh
+function UpgradePip {
+    return pip freeze | ForEach-Object { $_.split('==')[0] } | ForEach-Object { pip install --upgrade $_ };
+}
+
+function prompt {
+    $prompt = "Do you wish to update all existing pip-installed packages?`n==========================================================`nEnter `"1`" to continue with update process.`nEnter anything else to exit the process.`n`n> "
+    $uchoice = Read-Host -Prompt $prompt
+    if ($uchoice -eq "1") {
+        Write-Output "Upgrading ALL Pip Packages...`n"
+        UpgradePip
+        return Write-Output "`nAll eligible pip packages sucessfully upgraded!"
+    }
+    else {
+        return Write-Output "`nExiting window...`n";
+        exit
+    }
+}
+
+prompt;
