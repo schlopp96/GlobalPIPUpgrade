d693e4eeeb5fcaaca75a6110eb8cc1591c2521f9
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-04-16 21:31:51 -0500

    - Added new exception for KeyboardInterrupt errors.
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

--- UpgradePipPkgs/main.py
+++ UpgradePipPkgs/main.py
@@ -4,7 +4,7 @@ import subprocess
 from os import chdir
 from os.path import dirname
 from typing import NoReturn
-from apploggers import processLogger, mainLogger
+from applogging import processLogger, mainLogger
 from PyLoadBar import load
 
 chdir(dirname(__file__))
@@ -14,7 +14,7 @@ __version__ = '0.1.0'
 textborder: str = f'\n<{"*" * 120}>\n'
 
 def upgrade():
-    """Upgrade outdated pip packages.
+    """Upgrade global pip packages.
 
     ---
 
@@ -31,8 +31,8 @@ def upgrade():
             mainLogger.info('Successfully completed global PIP package upgrade!')
             return exitProgram(0)
         except subprocess.CalledProcessError as e:
-            mainLogger.warning(f'An error occurred during execution of upgrade subprocess:\n{str(e)}')
-            print(f'An error occurred during execution of upgrade subprocess:\n{str(e)}')
+            mainLogger.warning(f'An error occurred during execution of upgrade subprocess:\n--> {str(e)}')
+            print(f'An error occurred during execution of upgrade subprocess:\n--> {str(e)}')
             return exitProgram(1)
 
 
@@ -42,13 +42,13 @@ def exitProgram(exitcode: int) -> NoReturn | None:
     ---
 
     Parameters:
-        :param exitcode: reflects whether program exit was due to successful or failed operation.
+        :param exitcode: code reflecting whether program exit was due to successful or failed operation.
         :type exitcode: int
-        :return: exits program based on exit-code.
+        :return: exits program with passed `exitcode`.
         :rtype: NoReturn
     """
     mainLogger.info('Preparing to exit...')
-    load('Preparing to exit...', 'Exiting program...', enable_display=False)
+    load('Preparing to exit...', 'Exiting program...', 10, enable_display=False)
     mainLogger.info(f'Closing log file...{textborder}')
     return exit(exitcode)
 
@@ -66,16 +66,22 @@ def main() -> NoReturn | None:
     """
     mainLogger.info(f'Welcome to UpgradePipPkgs {__version__}!')
     while True:
-        mainLogger.info('Display upgrade prompt...')
+        mainLogger.info('Displaying menu prompt...')
         prompt: str = input(f'{"*"*50}\n| > Upgrade all globally installed PIP packages? |\n| > Enter [Y] to begin upgrade process.          |\n| > Enter [N] to exit.                           |\n{"*"* 50}\n>>> ')
         match prompt.lower():
             case 'y':
-                upgrade()
+                try:
+                    print()
+                    return upgrade()
+                except KeyboardInterrupt:
+                    mainLogger.warning(f'An error occurred during execution of upgrade subprocess:\n==> Keyboard interrupt was triggered by user.')
+                    print(f'\nAn error occurred during execution of upgrade subprocess:\n==> Keyboard interrupt was triggered by user...\n')
+                    return exitProgram(1)
             case 'n':
                 return exitProgram(0)
             case _:
-                mainLogger.warning(f'Incorrect response from input:\n> Incorrect response: "{prompt}".\n> Accepted values are limited to: "Y" or "N".\n> Please try again.')
-                print(f'\n> Incorrect response: "{prompt}".\n> Accepted values are limited to: "Y" or "N".\n> Please try again.\n')
+                mainLogger.warning(f'Incorrect response from input:\n==> Incorrect response: "{prompt}".\n==> Accepted values are limited to: "Y" or "N".\n==> Please try again.')
+                print(f'\n==> Incorrect response: "{prompt}".\n==> Accepted values are limited to: "Y" or "N".\n==> Please try again.\n')
 
 if __name__ == '__main__':
     main()
